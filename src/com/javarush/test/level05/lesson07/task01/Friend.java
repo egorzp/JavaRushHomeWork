205a19f887fe4ac833aec0d0cf42c854938c5945
egorzp <romanenkoee@gmail.com>
2014-11-08 15:12:32 +0200

    home work

---
+++ src/com/javarush/test/level05/lesson07/task01/Friend.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level05.lesson07.task01;
+
+/* Создать класс Friend
+Создать класс Friend (друг) с тремя инициализаторами (тремя методами initialize):
+- Имя
+- Имя, возраст
+- Имя, возраст, пол
+*/
+
+public class Friend
+{
+    String name;
+    int age;
+    boolean sex;
+    public void initialize(String name){
+        this.name = name;
+    }
+    public void initialize(String name, int age){
+        initialize(name);
+        this.age = age;
+    }
+    public void initialize(String name, int age, boolean sex){
+        initialize(name, age);
+        this.sex = sex;
+    }
+
+}
