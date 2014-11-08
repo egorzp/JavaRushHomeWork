d370140ddf759a5f021b6f54922b5aef3445c9c9
egorzp <romanenkoee@gmail.com>
2014-11-08 17:22:34 +0200

    home work

---
+++ src/com/javarush/test/level05/lesson09/task01/Friend.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level05.lesson09.task01;
+
+/* Создать класс Friend
+Создать класс Friend (друг) с тремя конструкторами:
+- Имя
+- Имя, возраст
+- Имя, возраст, пол
+*/
+
+public class Friend
+{
+    String name;
+    int age;
+    boolean sex;
+
+    public Friend(String name)
+    {
+        this.name = name;
+    }
+
+    public Friend(String name, int age)
+    {
+        this(name);
+        this.age = age;
+    }
+
+    public Friend(String name, boolean sex, int age)
+    {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+}
+
