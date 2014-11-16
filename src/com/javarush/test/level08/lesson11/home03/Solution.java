008e3589daafac09948521e27adad04780a6e773
egorzp <romanenkoee@gmail.com>
2014-11-16 19:45:56 +0200

    home_work_on_Linux

---
+++ src/com/javarush/test/level08/lesson11/home03/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.test.level08.lesson11.home03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* Люди с одинаковыми именами и/или фамилиями
+1. Создать словарь Map (<String, String>) и добавить туда 10 человек в виде «Фамилия»-«Имя».
+2. Пусть среди этих 10 человек есть люди с одинаковыми именами.
+3. Пусть среди этих 10 человек есть люди с одинаковыми фамилиями.
+4. Вывести содержимое Map на экран.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Map<String, String> map = createPeopleList();
+        printPeopleList(map);
+    }
+
+    public static Map<String, String> createPeopleList()
+    {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("surname1", "name3");
+        map.put("surname2", "name9");
+        map.put("surname3", "name8");
+        map.put("surname4", "name3");
+        map.put("surname5", "name5");
+        map.put("surname2", "name4");
+        map.put("surname7", "name3");
+        map.put("surname2", "name1");
+        map.put("surname9", "name2");
+        map.put("surname10", "name1");
+
+        return map;
+    }
+
+    public static void printPeopleList(Map<String, String> map)
+    {
+        for (Map.Entry<String, String> s : map.entrySet())
+        {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+
+}
